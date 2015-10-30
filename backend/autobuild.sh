29dd08c3dc1537b731790fc19d9a9e152ef1eaa3
dexter <dexter@dexterlabs.nl>
2015-10-30 16:34:34 +0100

    Created autobuilder

--- backend/autobuild.sh
+++ backend/autobuild.sh
@@ -1,5 +1,32 @@
-#!/bin/sh
+#!/bin/bash
+
+# trap ctrl-c and call ctrl_c()
+trap stop_runner INT
+
+
+function build_run() {
+	cargo build
+	kill_runner
+	cargo run &
+}
+
+function stop_runner() {
+	kill_runner
+	echo "Stopping"
+	exit 1
+}
+
+function kill_runner() {
+	# Kill cargo run
+	# kill $(ps aux | grep 'cargo run' | awk ' {print $2}')
+	kill $(ps aux | grep 'target/debug/backend' | awk ' {print $2}')
+}
+
+cargo build
+cargo run &
 
 while inotifywait -r -e close_write src/*; 
-do   cargo build && run; 
+do
+	build_run
 done
+
