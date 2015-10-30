6c86d95a36613bfec09b74c38c06fc91f0124178
Benjamin W. Broersma <bwb@bwblabs.com>
2015-10-30 17:05:42 +0100

    Random

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,63 +9,46 @@ use persistent::Read;
 use iron::status;
 use iron::prelude::*;
 use router::{Router};
-use crypto::digest::Digest;
-use crypto::sha2::Sha256;
+//use crypto::digest::Digest;
+//use crypto::sha2::Sha256;
+//use crypto::util::fixed_time_eq
 
 
-/*#[derive(Debug, Clone, RustcDecodable)]
-struct MyStructure {
-    name: String,
-    age: Option<String>,
-}*/
-
-fn log_body(req: &mut Request) -> IronResult<Response> {
-
-    Ok(Response::with(status::Ok))
+#[derive(Debug, Clone, RustcDecodable)]
+struct Login {
+    user: String,
+    password: String,
 }
-    //let mut hasher = Sha256::new();
-    //hasher.input_str("hello world");
-    //let hex = hasher.result_str();
-
+//let mut hasher = Sha256::new();
+//hasher.input_str("hello world");
+//let hex = hasher.result_str();
 
 const MAX_BODY_LENGTH: usize = 1024 * 1024 * 10;
+
+fn handleLogin(login: Login) -> IronResult<Response> {
+    if login.user == "invalid" {
+        Ok(Response::with((status::ImATeapot, "test")))
+    } else {
+    //$msg = array('timestamp' => time(), 'id' => 42);
+    //response(array('message' => $msg, 'signature' => hash_hmac('sha256', json_encode($msg), $secretKey)));
+        Ok(Response::with((status::Ok)))
+    }
+}
 
 // `curl -i "localhost:3000/" -H "application/json" -d '{"name":"jason","age":"2"}'`
 // and check out the printed json in your terminal.
 fn main() {
     let mut router = Router::new();  // Alternative syntax:
-    router.get("/", handler);        // let router = router!(get "/" => handler,
-    router.post("/:query", handler);  //                      get "/:query" => handler);
+    //router.get("/", handler);        // let router = router!(get "/" => handler,
+    router.post("/token", token);  //                      get "/:query" => handler);
 
-    fn handler(req: &mut Request) -> IronResult<Response> {
-        //let ref query = req.extensions.get::<Router>()
-        //    .unwrap().find("query").unwrap_or("/");
-
-
-        let body = req.get::<bodyparser::Raw>();
-        /*match body {
-            Ok(Some(body)) => println!("Read body:\n{}", body),
-            Ok(None) => println!("No body"),
-            Err(err) => println!("Error: {:?}", err)
-        }*/
-
-        let json_body = req.get::<bodyparser::Json>();
-        match json_body {
-            Ok(Some(json_body)) => println!("Parsed body:\n{:?}", json_body),
-            Ok(None) => println!("No body"),
-            Err(err) => println!("Error: {:?}", err)
+    fn token(req: &mut Request) -> IronResult<Response> {
+        let login = req.get::<bodyparser::Struct<Login>>();
+        match login {
+            Ok(Some(login)) => handleLogin(login),//println!("Parsed body:\n{:?}", login),
+            Ok(None) => Ok(Response::with((status::BadRequest))),//println!("No postdata"),
+            Err(err) => Ok(Response::with((status::BadRequest)))//println!("Error: {:?}", err)
         }
-
-        /*let struct_body = req.get::<bodyparser::Struct<MyStructure>>();
-        match struct_body {
-            Ok(Some(struct_body)) => println!("Parsed body:\n{:?}", struct_body),
-            Ok(None) => println!("No body"),
-            Err(err) => println!("Error: {:?}", err)
-        }*/
-
-
-        //Ok(Response::with((status::Ok, *query)))
-        Ok(Response::with((status::Ok)))
     }
 
     let mut chain = Chain::new(router);
