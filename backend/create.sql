281a29f1c715455e027326e649c480b3d34d9ad2
Benjamin W. Broersma <bwb@bwblabs.com>
2015-11-26 22:30:24 +0100

    New Dexter DB schema.

--- backend/create.sql
+++ backend/create.sql
@@ -3,17 +3,21 @@ BEGIN;
 CREATE OR REPLACE FUNCTION update_timestamp() RETURNS trigger AS
 $$ BEGIN NEW.modified = NOW(); RETURN NEW; END; $$ LANGUAGE plpgsql;
 
+DROP SEQUENCE IF EXISTS "gid_seq" CASCADE;
+CREATE SEQUENCE "gid_seq" INCREMENT 1 MINVALUE 1 START 1 CACHE 1;
+
+
 DROP TABLE IF EXISTS "members" CASCADE;
 CREATE TABLE "members"
 (
-    "gid"               SERIAL,
+    "gid"               INT NOT NULL DEFAULT NEXTVAL('"gid_seq"'),
     "id"                SERIAL,
     "valid_from"        TIMESTAMPTZ DEFAULT NOW() NOT NULL CHECK ("valid_from" < "valid_till"),
     "valid_till"        TIMESTAMPTZ,
     "email"             VARCHAR(255),
     "phone"             VARCHAR(255),
     "password_hash"     VARCHAR(255),
-    "data"              JSONB,
+    "data"              JSONB NOT NULL DEFAULT '{}',
     "modified_by"       INT NOT NULL,
     "modified"          TIMESTAMPTZ, -- this should always be NULL if we don't do manual SQL
     "created"           TIMESTAMPTZ DEFAULT NOW() NOT NULL
@@ -23,14 +27,15 @@ WITH (
 );
 CREATE TRIGGER "members_modified" BEFORE UPDATE ON "members" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
-DROP TABLE IF EXISTS "roles" CASCADE;
-CREATE TABLE "roles"
+DROP TABLE IF EXISTS "groups" CASCADE;
+CREATE TABLE "groups"
 (
-    "gid"               SERIAL,
+    "gid"               INT NOT NULL DEFAULT NEXTVAL('"gid_seq"'),
     "id"                SERIAL,
     "valid_from"        TIMESTAMPTZ DEFAULT NOW() NOT NULL CHECK ("valid_from" < "valid_till"),
     "valid_till"        TIMESTAMPTZ,
     "name"              VARCHAR(255) NOT NULL,
+    "data"              JSONB NOT NULL DEFAULT '{}',
     "modified_by"       INT NOT NULL,
     "modified"          TIMESTAMPTZ, -- this should always be NULL if we don't do manual SQL
     "created"           TIMESTAMPTZ DEFAULT NOW() NOT NULL
@@ -38,16 +43,16 @@ CREATE TABLE "roles"
 WITH (
     OIDS=FALSE
 );
-CREATE TRIGGER "roles_modified" BEFORE UPDATE ON "roles" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
+CREATE TRIGGER "groups_modified" BEFORE UPDATE ON "groups" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
-DROP TABLE IF EXISTS "members_roles" CASCADE;
-CREATE TABLE "members_roles"
+DROP TABLE IF EXISTS "members_groups" CASCADE;
+CREATE TABLE "members_groups"
 (
-    "gid"               SERIAL,
+    "gid"               INT NOT NULL DEFAULT NEXTVAL('"gid_seq"'),
     "valid_from"        TIMESTAMPTZ DEFAULT NOW() NOT NULL CHECK ("valid_from" < "valid_till"),
     "valid_till"        TIMESTAMPTZ,
     "members_id"        INT NOT NULL,
-    "roles_id"          INT NOT NULL,
+    "groups_id"          INT NOT NULL,
     "modified_by"       INT NOT NULL,
     "modified"          TIMESTAMPTZ, -- this should always be NULL if we don't do manual SQL
     "created"           TIMESTAMPTZ DEFAULT NOW() NOT NULL
@@ -55,17 +60,18 @@ CREATE TABLE "members_roles"
 WITH (
     OIDS=FALSE
 );
-CREATE TRIGGER "members_roles_modified" BEFORE UPDATE ON "members_roles" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
+CREATE TRIGGER "members_groups_modified" BEFORE UPDATE ON "members_roles" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
 DROP TABLE IF EXISTS "fields" CASCADE;
 CREATE TABLE "fields"
 (
-    "gid"               SERIAL,
+    "gid"               INT NOT NULL DEFAULT NEXTVAL('"gid_seq"'),
     "id"                SERIAL,
     "valid_from"        TIMESTAMPTZ DEFAULT NOW() NOT NULL CHECK ("valid_from" < "valid_till"),
     "valid_till"        TIMESTAMPTZ,
+    "ref_type"          VARCHAR(255) NOT NULL,
     "name"              VARCHAR(255) NOT NULL,
-    "data"              JSONB,
+    "data"              JSONB NOT NULL DEFAULT '{}',
     "modified_by"       INT NOT NULL,
     "modified"          TIMESTAMPTZ, -- this should always be NULL if we don't do manual SQL
     "created"           TIMESTAMPTZ DEFAULT NOW() NOT NULL
@@ -75,16 +81,21 @@ WITH (
 );
 CREATE TRIGGER "fields_modified" BEFORE UPDATE ON "fields" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
-DROP TABLE IF EXISTS "fields_roles" CASCADE;
-CREATE TABLE "fields_roles"
+DROP TYPE IF EXISTS "permissions_type";
+CREATE TYPE "permissions_type" AS ENUM ('read', 'write', 'grant');
+
+DROP TABLE IF EXISTS "permissions" CASCADE;
+CREATE TABLE "permissions"
 (
-    "gid"               SERIAL,
+    "gid"               INT NOT NULL DEFAULT NEXTVAL('"gid_seq"'),
+    "id"                SERIAL,
     "valid_from"        TIMESTAMPTZ DEFAULT NOW() NOT NULL CHECK ("valid_from" < "valid_till"),
     "valid_till"        TIMESTAMPTZ,
-    "read"              BOOLEAN DEFAULT TRUE,
-    "write"             BOOLEAN DEFAULT FALSE,
-    "fields_id"         INT NOT NULL,
-    "roles_id"          INT NOT NULL,
+    "name"              VARCHAR(255) NOT NULL,
+    "type"              permissions_type NOT NULL DEFAULT 'read',
+    "ref_type"          VARCHAR(255) NOT NULL,
+    "ref_id"            INT NOT NULL,
+    "data"              JSONB NOT NULL DEFAULT '{}',
     "modified_by"       INT NOT NULL,
     "modified"          TIMESTAMPTZ, -- this should always be NULL if we don't do manual SQL
     "created"           TIMESTAMPTZ DEFAULT NOW() NOT NULL
@@ -92,8 +103,26 @@ CREATE TABLE "fields_roles"
 WITH (
     OIDS=FALSE
 );
-CREATE TRIGGER "fields_roles_modified" BEFORE UPDATE ON "fields_roles" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
+CREATE TRIGGER "permissions_modified" BEFORE UPDATE ON "permissions" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
---FIXME: PLEASE ADD INDEXING
+
+DROP TABLE IF EXISTS "groups_permissions" CASCADE;
+CREATE TABLE "groups_permissions"
+(
+    "gid"               INT NOT NULL DEFAULT NEXTVAL('"gid_seq"'),
+    "valid_from"        TIMESTAMPTZ DEFAULT NOW() NOT NULL CHECK ("valid_from" < "valid_till"),
+    "valid_till"        TIMESTAMPTZ,
+    "groups_id"         INT NOT NULL,
+    "permissions_id"    INT NOT NULL,
+    "modified_by"       INT NOT NULL,
+    "modified"          TIMESTAMPTZ, -- this should always be NULL if we don't do manual SQL
+    "created"           TIMESTAMPTZ DEFAULT NOW() NOT NULL
+)
+WITH (
+    OIDS=FALSE
+);
+CREATE TRIGGER "groups_permissions_modified" BEFORE UPDATE ON "groups_permissions" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
+
+--FIXME: PLEASE ADD INDEXING AND PRIMARY KEYS ;)
 
 COMMIT;
