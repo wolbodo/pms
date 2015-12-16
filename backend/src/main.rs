5a37141a0fa79d366b594b71f281589897d56509
dexter <dexter@dexterlabs.nl>
2015-12-16 17:16:44 +0100

    Creating database

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,11 +10,11 @@ extern crate crypto;
 extern crate postgres;
 extern crate iron_postgres_middleware as pg_middleware;
 
-use persistent::Read;
+// use persistent::Read;
 use iron::status;
 use iron::prelude::*;
-use router::{Router};
-use std::collections::BTreeMap;
+// use router::{Router};
+// use std::collections::BTreeMap;
 use serde_json::*;
 //use postgres::{Connection, SslMode};
 use pg_middleware::{PostgresMiddleware, PostgresReqExt};
@@ -44,7 +44,7 @@ struct Login {
 // }
 
 
-const MAX_BODY_LENGTH: usize = 1024 * 1024 * 10;
+// const MAX_BODY_LENGTH: usize = 1024 * 1024 * 10;
 
 fn handle_login(req: &mut Request) -> IronResult<Response> {
     /*let login = match req.get::<bodyparser::Struct<Login>>() {
@@ -97,49 +97,49 @@ fn handle_login(req: &mut Request) -> IronResult<Response> {
 }
 
 fn handle_members(req: &mut Request) -> IronResult<Response> {
-    // // Returns a list of members, might be using filters. 
+    // Returns a list of members, might be using filters. 
 
-    // let db = req.db_conn();
-    // let stmt = db.prepare("
-    //     WITH readfields (key, selfid) AS (
-    //         SELECT DISTINCT fields.name, CASE WHEN groups.name = 'self' THEN people.id END FROM
-    //             fields JOIN permissions ON  permissions.ref_key = 'field' AND permissions.ref_value = fields.id AND permissions.valid_till IS NULL AND fields.valid_till IS NULL
-    //                    JOIN groups_permissions ON permissions.id = groups_permissions.permissions_id AND groups_permissions.valid_till IS NULL
-    //                    JOIN groups ON groups.id = groups_permissions.groups_id AND groups.valid_till IS NULL
-    //                    JOIN people_groups ON (people_groups.groups_id = groups.id OR groups.name = 'self') AND people_groups.valid_till IS NULL
-    //                    JOIN people ON people_groups.people_id = people.id AND people.valid_till IS NULL
-    //             WHERE permissions.type = 'read' AND permissions.ref_type = 'people' AND people.id = $1
-    //     )
-    //     SELECT ('{' || (
-    //         SELECT STRING_AGG('\"' || key || '\":' || TO_JSON(value), ',')
-    //         FROM (SELECT * FROM JSONB_EACH(data) UNION
-    //             VALUES
-    //                 ('gid'::TEXT, TO_JSON(gid)::JSONB),
-    //                 ('id', TO_JSON(id)::JSONB),
-    //                 ('valid_from', TO_JSON(FLOOR(EXTRACT(EPOCH FROM valid_from)))::JSONB),
-    //                 ('valid_till', COALESCE(TO_JSON(FLOOR(EXTRACT(EPOCH FROM valid_till)))::JSONB, 'null'::JSONB)),
-    //                 ('email', COALESCE(TO_JSON(email)::JSONB, 'null'::JSONB)),
-    //                 ('phone', COALESCE(TO_JSON(phone)::JSONB, 'null'::JSONB)),
-    //                 ('password_hash', COALESCE(TO_JSON(password_hash)::JSONB, 'null'::JSONB)),
-    //                 ('modified_by', TO_JSON(modified_by)::JSONB),
-    //                 ('modified', COALESCE(TO_JSON(FLOOR(EXTRACT(EPOCH FROM modified)))::JSONB, 'null'::JSONB)),
-    //                 ('created', TO_JSON(FLOOR(EXTRACT(EPOCH FROM created)))::JSONB)
-    //             ) alias
-    //             WHERE key IN (SELECT key FROM readfields WHERE selfid IS NULL OR people.id = selfid))  || '}')::JSONB
-    //         FROM people WHERE valid_till IS NULL;"
-    //     ).unwrap();
+    let db = req.db_conn();
+    let stmt = db.prepare("
+        WITH readfields (key, selfid) AS (
+            SELECT DISTINCT fields.name, CASE WHEN groups.name = 'self' THEN people.id END FROM
+                fields JOIN permissions ON  permissions.ref_key = 'field' AND permissions.ref_value = fields.id AND permissions.valid_till IS NULL AND fields.valid_till IS NULL
+                       JOIN groups_permissions ON permissions.id = groups_permissions.permissions_id AND groups_permissions.valid_till IS NULL
+                       JOIN groups ON groups.id = groups_permissions.groups_id AND groups.valid_till IS NULL
+                       JOIN people_groups ON (people_groups.groups_id = groups.id OR groups.name = 'self') AND people_groups.valid_till IS NULL
+                       JOIN people ON people_groups.people_id = people.id AND people.valid_till IS NULL
+                WHERE permissions.type = 'read' AND permissions.ref_type = 'people' AND people.id = $1
+        )
+        SELECT ('{' || (
+            SELECT STRING_AGG('\"' || key || '\":' || TO_JSON(value), ',')
+            FROM (SELECT * FROM JSONB_EACH(data) UNION
+                VALUES
+                    ('gid'::TEXT, TO_JSON(gid)::JSONB),
+                    ('id', TO_JSON(id)::JSONB),
+                    ('valid_from', TO_JSON(FLOOR(EXTRACT(EPOCH FROM valid_from)))::JSONB),
+                    ('valid_till', COALESCE(TO_JSON(FLOOR(EXTRACT(EPOCH FROM valid_till)))::JSONB, 'null'::JSONB)),
+                    ('email', COALESCE(TO_JSON(email)::JSONB, 'null'::JSONB)),
+                    ('phone', COALESCE(TO_JSON(phone)::JSONB, 'null'::JSONB)),
+                    ('password_hash', COALESCE(TO_JSON(password_hash)::JSONB, 'null'::JSONB)),
+                    ('modified_by', TO_JSON(modified_by)::JSONB),
+                    ('modified', COALESCE(TO_JSON(FLOOR(EXTRACT(EPOCH FROM modified)))::JSONB, 'null'::JSONB)),
+                    ('created', TO_JSON(FLOOR(EXTRACT(EPOCH FROM created)))::JSONB)
+                ) alias
+                WHERE key IN (SELECT key FROM readfields WHERE selfid IS NULL OR people.id = selfid))  || '}')::JSONB
+            FROM people WHERE valid_till IS NULL;"
+        ).unwrap();
 
-    // let rows = stmt.query(&[&3]).unwrap();
+    let rows = stmt.query(&[&3]).unwrap();
 
     let mut members: Vec<Value> = Vec::new();
 
-    // for row in rows {
+    for row in rows {
 
-    //     let data: Value = row.get("jsonb");
-    //     println!("{:?}", data);
+        let data: Value = row.get("jsonb");
+        println!("{:?}", data);
 
-    //     members.push(data);
-    // }
+        members.push(data);
+    }
 
     Ok(Response::with((status::Ok, serde_json::to_string(&members).unwrap())))
     // // Err(Response::with((status::Ok)));
@@ -186,12 +186,12 @@ fn main() {
 
     let mut chain = Chain::new(router);
 
-    // println!("Connecting to database.");
+    println!("Connecting to database.");
     // for unix domain sockets use: %2Frun%2Fpostgresql%2F9.4-main.pid
-    // let pg_middleware = PostgresMiddleware::new("postgres://pms@127.0.0.1/pms");
-    // println!("Connected.");
+    let pg_middleware = PostgresMiddleware::new("postgres://postgres@postgres/pms");
+    println!("Connected.");
 
-    // chain.link_before(pg_middleware);
+    chain.link_before(pg_middleware);
     //chain.link_before(Read::<bodyparser::MaxBodyLength>::one(MAX_BODY_LENGTH));
     Iron::new(chain).http("0.0.0.0:4242").unwrap();
 }
