86f137acc02036097b340e821db48a165ea46e8d
dexter <dexter@dexterlabs.nl>
2016-04-11 22:17:51 +0200

    Added roles_set

--- database/db-logic.sql
+++ database/db-logic.sql
@@ -441,6 +441,33 @@ END;
 $function$;
 
 
+CREATE OR REPLACE FUNCTION public.roles_set(token TEXT, roles_id INT, data JSONB)
+ RETURNS JSONB
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+    rights payload_permissions;
+    _data ALIAS FOR data;
+BEGIN
+    rights = permissions_get(token);
+    _data = remove_base(data_merge(
+        rights := rights,
+        ref_table := 'roles',
+        base := roles_get(rights, roles_id),
+        update := _data
+    ));
+
+    UPDATE roles SET valid_till = NOW() WHERE id = roles_id AND valid_till IS NULL;
+
+    INSERT INTO roles (id, valid_from, name, modified_by, data)
+        SELECT id, valid_till, _data->>'name', (rights.payload->>'user')::INT, _data
+            FROM roles WHERE id = roles_id ORDER BY valid_till DESC LIMIT 1;
+
+    RETURN roles_get(rights, roles_id);
+END;
+$function$;
+
+
 --NOTE: ONLY expose this function internally! (because Dexter only wants to expose roles to people who can log in)
 CREATE OR REPLACE FUNCTION public.fields_get(rights payload_permissions, ref_table VARCHAR(255) DEFAULT NULL)
  RETURNS JSONB
