b58da21aafe98909e9f5307b7fa979840daa4168
Dexter <dexter@dexterlabs.nl>
2017-01-04 01:13:50 +0100

    All tests passing again

--- backend-rocket/src/main.rs
+++ backend-rocket/src/main.rs
@@ -15,6 +15,8 @@ extern crate lazy_static;
 extern crate r2d2;
 extern crate r2d2_postgres;
 
+use std::str::FromStr;
+
 use serde_json::Value;
 use r2d2_postgres::{TlsMode, PostgresConnectionManager};
 use r2d2_postgres::postgres::error::Error as PgError;
@@ -22,6 +24,7 @@ use r2d2_postgres::postgres::error::Error as PgError;
 use rocket::Outcome;
 use rocket::http::Status;
 use rocket::request::{self, Request, FromRequest};
+use rocket::response::status;
 use rocket_contrib::JSON;
 
 lazy_static! {
@@ -37,8 +40,8 @@ macro_rules! db_call {
 
     let rows = match stmt.query($args) {
         Ok(rows) => rows,
-        Err(PgError::Db(err)) => return Err(err.message),
-        Err(err) => return Err(err.to_string()),
+        Err(PgError::Db(err)) => return Err(status::Custom(Status::BadRequest, err.message)),
+        Err(err) => return Err(status::Custom(Status::InternalServerError, err.to_string())),
     };
 
     let object: Value = match rows.get(0).get(0) {
@@ -59,7 +62,6 @@ fn connection() -> r2d2::PooledConnection<r2d2_postgres::PostgresConnectionManag
     let pool = DB_POOL.clone();
     pool.get().unwrap()
 }
-
 
 struct APIContext {
   token: String
@@ -94,124 +96,127 @@ struct Login {
 }
 
 #[post("/login", data = "<login>")]
-fn login(login: JSON<Login>) -> Result<JSON<Value>, String> {
+fn login(login: JSON<Login>) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT login(emailaddress := $1, password := $2);",
     &[&login.0.user, &login.0.password],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[post("/people", data = "<person>")]
-fn people_add(api: APIContext, person: JSON<Value>) -> Result<JSON<Value>, String> {
+fn people_add(api: APIContext, person: JSON<Value>) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT people_add(token := $1, data := $2);",
     &[&api.token, &person.0],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[get("/people")]
-fn people_get(api: APIContext) -> Result<JSON<Value>, String> {
+fn people_get(api: APIContext) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT people_get(token := $1);",
     &[&api.token],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[get("/people/<id>")]
-fn people_get_i(api: APIContext, id: i32) -> Result<JSON<Value>, String> {
+fn people_get_i(api: APIContext, id: i32) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT people_get(token := $1, people_id := $2);",
     &[&api.token, &id],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[put("/people/<id>", data = "<person>")]
-fn people_set_i(api: APIContext, id: i32, person: JSON<Value>) -> Result<JSON<Value>, String> {
+fn people_set_i(api: APIContext, id: i32, person: JSON<Value>) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT people_set(token := $1, people_id := $2, data := $3);",
     &[&api.token, &id, &person.0],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[post("/roles", data = "<role>")]
-fn roles_add(api: APIContext, role: JSON<Value>) -> Result<JSON<Value>, String> {
+fn roles_add(api: APIContext, role: JSON<Value>) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT roles_add(token := $1, data := $2);",
     &[&api.token, &role.0],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[get("/roles")]
-fn roles_get(api: APIContext) -> Result<JSON<Value>, String> {
+fn roles_get(api: APIContext) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT roles_get(token := $1);",
     &[&api.token],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[get("/roles/<id>")]
-fn roles_get_i(api: APIContext, id: i32) -> Result<JSON<Value>, String> {
+fn roles_get_i(api: APIContext, id: i32) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT roles_get(token := $1, roles_id := $2);",
     &[&api.token, &id],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[put("/roles/<id>", data = "<role>")]
-fn roles_set(api: APIContext, id: i32, role: JSON<Value>) -> Result<JSON<Value>, String> {
+fn roles_set(api: APIContext, id: i32, role: JSON<Value>) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT roles_set(token := $1, roles_id := $2, data := $3);",
     &[&api.token, &id, &role.0],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[get("/permissions")]
-fn permissions_get(api: APIContext) -> Result<JSON<Value>, String> {
+fn permissions_get(api: APIContext) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT roles_permissions_get(token := $1);",
     &[&api.token],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[get("/fields")]
-fn fields_get(api: APIContext) -> Result<JSON<Value>, String> {
+fn fields_get(api: APIContext) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT fields_get(token := $1);",
     &[&api.token],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[get("/fields/<table>")]
-fn fields_get_i(api: APIContext, table: String) -> Result<JSON<Value>, String> {
+fn fields_get_i(api: APIContext, table: String) -> Result<JSON<Value>, status::Custom<String>> {
   db_call!(
     "SELECT fields_get(token := $1, ref_table := $2);",
     &[&api.token, &table],
-    "Id not found (or no read access)".to_string()
+    status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   )
 }
 
 #[put("/fields")]
-fn fields_set(api: APIContext) -> Result<JSON<Value>, String> {
+fn fields_set(api: APIContext) -> Result<JSON<Value>, status::Custom<String>> {
   Ok(JSON(Value::Bool(false)))
   // db_call!(
     // "SELECT fields_set(token := $1, people_id := $2, data := $3);",
     // &[&api.token, &id, &person.0],
-    // "Id not found (or no read access)".to_string()
+    // status::Custom(Status::InternalServerError, "Id not found (or no read access)".to_string())
   // )
 }
 
-
+#[error(400)]
+fn badrequest() -> JSON<Value> { 
+  JSON(Value::from_str("{\"error\":\"No Authorization header found\"}").unwrap())
+}
 
 fn main() {
     rocket::ignite().mount("/", routes![
@@ -228,5 +233,7 @@ fn main() {
       fields_get,
       fields_get_i,
       fields_set,
-    ]).launch();
+    ])
+    .catch(errors![badrequest])
+    .launch();
 }
